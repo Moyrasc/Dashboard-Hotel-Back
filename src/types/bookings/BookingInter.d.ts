1281cd50ba6865d12bb21b87c2e8f8057ccec765
Moyrasc <noemisolac@gmail.com>
2023-03-14 15:19:17 +0100

    create interfaces

---
+++ src/types/bookings/BookingInter.d.ts
@@ -0,0 +1,16 @@
+export interface BookingInter {
+    id: number;
+    guest: string;
+    orderDate: string;
+    checkin: string;
+    checkout: string;
+    roomId?: number;
+    price: number;
+    specialRequest: string;
+    amenities?: string[];
+    typeRoom: string;
+    description: string;
+    photo: {};
+    state: string
+
+}
---
+++ src/types/contacts/ContactInter.d.ts
@@ -0,0 +1,11 @@
+export interface ContactInter {
+    id: number;
+    date: string;
+    customer: string;
+    email: string;
+    phone: string;
+    subject: string;
+    comment: string;
+    actionPublish: string;
+    actionArchived: string;
+}
