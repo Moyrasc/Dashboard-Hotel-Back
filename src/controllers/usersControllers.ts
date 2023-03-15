fe2aee659de502c71a58a05a66fdcb86d68be64b
Moyrasc <noemisolac@gmail.com>
2023-03-15 18:20:23 +0100

    user controller and user routes completed

--- src/controllers/usersControllers.ts
+++ src/controllers/usersControllers.ts
@@ -1,15 +1,24 @@
-export const getUsers = async (req:Request, res: Response) =>{
+import users from '../data/users.json'
+import { UserInter } from "../types/users/UserInter"
+
+export const getAllUsers = async (): Promise<UserInter[]> =>{
+  return users
 
 }
-export const getUser = async (req:Request, res: Response) =>{
+export const getUser = async (id: number): Promise<UserInter | null> =>{
+  const user = await users.find(item => item.id === Number(id))
+  return user ? user : null
 
 }
-export const editUser = async (req: Request, res: Response) =>{
+export const editUser = async (user: UserInter): Promise<UserInter | undefined> =>{
+  const updateUser = await users.find(item=> item.id === user.id)
+  return updateUser
 
 }
-export const newUser = async (req: Request, res: Response) =>{
-
+export const newUser = async (user: UserInter): Promise<UserInter> =>{
+  return user
 }
-export const deleteUser = async (req: Request, res: Response)=> {
-  
+export const deleteUser = async (id: number): Promise <UserInter | undefined> => {
+  const deleteUs = await users.find(item => item.id === Number(id))
+  return deleteUs
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,15 +1,60 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { deleteUser, editUser, getAllUsers, getUser, newUser } from '../controllers/usersControllers';
 
 const routerUsers = express.Router();
 
-routerUsers.get('/users');
+routerUsers.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await getAllUsers()
+    if (!users) throw new Error("not found")
+    res.send(users)
+  } catch (error) {
+    next(error)
+  }
+});
 
-routerUsers.get('/:userId');
+routerUsers.get('/:userId', async (req: Request, res: Response, next: NextFunction)=>{
+  const {userId} = req.params
+  try {
+    const user = await getUser(Number(userId))
+    !user
+      ? res.status(404).send({ msg: 'User not found' })
+      : res.send(user)
+  } catch (error) {
+    next(error)
+  }
+});
 
-routerUsers.post('/newUser');
+routerUsers.post('/newUser', async (req: Request, res: Response, next: NextFunction)=>{
+  const user = req.body
+  try {
+    const userNew = await newUser(user)
+    res.send(userNew)
+  } catch (error) {
+    next(error)
+  }
+});
 
-routerUsers.put('/editUser/:userId');
+routerUsers.put('/editUser/:userId', async (req: Request, res: Response, next: NextFunction)=>{
+  const user = req.body
+  try {
+    const updateUser = await editUser(user)
+    res.send(updateUser)
+  } catch (error) {
+    next(error)
+  }
+});
 
-routerUsers.delete('/:userId');
+routerUsers.delete('/id/:userId', async (req: Request, res: Response, next: NextFunction)=>{
+  const { userId } = req.params
+  try {
+    const deleteUs = await deleteUser(Number(userId))
+    !deleteUs
+      ? res.status(404).send({ msg: 'user not found' })
+      : res.send(deleteUs)
+  } catch (error) {
+    next(error)
+  }
+});
 
 export default routerUsers
